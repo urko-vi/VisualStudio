587d03423cb83b69818e143487d160d376b1bbd6
urko-vi <urko.informatica@gmail.com>
2015-09-23 17:30:22 +0200

    Factoria Alumnos

---
+++ GestorAlumnos/FactoriaAlumnos.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorAlumnos
+{
+    class FactoriaAlumnos
+    {
+        IList<Alumno> alumnos = null;
+        //MostrarAlumnos
+        public IList<Alumno> MostrarAlumnos()
+        {
+            alumnos = new List<Alumno>();
+            return alumnos;
+        }
+        //AñadirAlumnos
+
+    }
+}
