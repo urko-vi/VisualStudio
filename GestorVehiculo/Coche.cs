157b112b489c9ea2d89b3b4545b0fd90bb542d10
urko-vi <urko.informatica@gmail.com>
2015-09-24 17:00:06 +0200

    Proyecto Gestor Coche Añadido

---
+++ GestorVehiculo/Coche.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestorVehiculo
+{
+    class Coche
+    {
+        private string nBastidor;
+        private string marca;
+        private string modelo;
+        private int nVelocidad;
+        private DateTime fMatriculacion;
+
+
+        public Coche()
+        {
+            NBastidor = "";
+            Marca = "";
+            Modelo = "";
+            NVelocidad = 0;
+            FMatriculacion = DateTime.Now;
+        }
+
+        public string NBastidor
+        {
+            get
+            {
+                return nBastidor;
+            }
+
+            set
+            {
+                nBastidor = value;
+            }
+        }
+
+        public string Marca
+        {
+            get
+            {
+                return marca;
+            }
+
+            set
+            {
+                marca = value;
+            }
+        }
+
+        public string Modelo
+        {
+            get
+            {
+                return modelo;
+            }
+
+            set
+            {
+                modelo = value;
+            }
+        }
+
+        public int NVelocidad
+        {
+            get
+            {
+                return nVelocidad;
+            }
+
+            set
+            {
+                nVelocidad = value;
+            }
+        }
+
+        public DateTime FMatriculacion
+        {
+            get
+            {
+                return fMatriculacion;
+            }
+
+            set
+            {
+                fMatriculacion = value;
+            }
+        }
+    }
+}
